93d1e489e4e8a924125ae08232c1af9b4b612d47
vutran <vu.tran@rudasoft.com>
2024-03-25 10:21:26 +0700

    update login validate

--- src/utils/vailidate.ts
+++ src/utils/vailidate.ts
@@ -1,5 +1,16 @@
 import * as yup from "yup";
 
+export const LoginSchema = yup.object().shape({
+  email: yup
+    .string()
+    .required("This field is required.")
+    .email("Email is invalid."),
+  password: yup
+    .string()
+    .min(6, "Password must be at least 6 characters")
+    .required("This field is required."),
+});
+
 export const signUpSchema = yup.object().shape({
   email: yup
     .string()
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+import { Form } from 'vee-validate';
+import BaseInput from '../components/BaseInput/BaseInput.vue';
+import { LoginSchema } from '../utils/vailidate';
+
+const onSubmit = (formData: any) => {
+  console.log('formData', formData);
+}
+</script>
+
 <template>
   <section class=" h-full pt-20">
     <div class="flex flex-col items-center justify-start px-6 mx-auto md:h-screen lg:py-0">
@@ -6,18 +16,12 @@
           <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
             Sign up
           </h1>
-          <form class="space-y-4" action="#">
+          <Form class="space-y-4" @submit="onSubmit" :validation-schema="LoginSchema">
             <div>
-              <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Your
-                email</label>
-              <input type="email" name="email" id="email"
-                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                placeholder="account@gmail.com">
+              <BaseInput name="email" type="email" label="Your email" placeholder="account@gmail.com" required />
             </div>
             <div>
-              <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
-              <input type="password" name="password" id="password" placeholder="••••••••"
-                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
+              <BaseInput name="password" type="password" label="Password" placeholder="••••••••" required />
             </div>
             <button type="submit"
               class="w-full btn-color text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Submit</button>
@@ -44,10 +48,8 @@
                   <span class="hidden md:block">Sign in with Facebook</span>
                 </div>
               </button>
-
             </div>
-
-          </form>
+          </Form>
         </div>
       </div>
     </div>
